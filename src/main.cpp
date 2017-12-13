2ff3f9a50e63d18f6065a686e1f0cf9ddc66388b
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2017-12-13 14:12:25 -0500

    Have trouble compiling library, works fine otherwise. Do digital multiplication ok.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <stdint.h>
+#include <stdio.h>
 
 const char *ssid = "T2_4";
 const char *password = "group224";
@@ -13,8 +14,8 @@ const char WiFiAPPSK[] = "esp";
 /////////////////////
 // Pin Definitions //
 /////////////////////
-const int LED_PIN = 5;      // Thing's onboard, green LED
-const int ANALOG_PIN = A0;  // The only analog pin on the Thing
+const int LED_PIN = 5;     // Thing's onboard, green LED
+const int ANALOG_PIN = A0; // The only analog pin on the Thing
 
 WiFiServer server(80);
 
@@ -50,28 +51,42 @@ void initHardware() {
   // that's all it can be.
 }
 
-static constexpr uint16_t BUFFER_SIZE = 60;
+int debug_vprintf(const char *format, va_list a) {
+  static constexpr uint8_t BUFFER_SIZE = 255;
+  static char Buffer[BUFFER_SIZE];
+  Serial.println(vsnprintf(Buffer, BUFFER_SIZE, format, a));
+  return 1;
+} // debug_vprintf
 
-static struct {
-  uint16_t ADC;
-  uint32_t Time;
-} MeasBuffer[BUFFER_SIZE], *CurEntry = MeasBuffer;
-
-static void measurement() {
-  if (CurEntry != MeasBuffer + BUFFER_SIZE) {
-    CurEntry->ADC = analogRead(ANALOG_PIN);
-    CurEntry->Time = micros();
-    CurEntry++;
-  }
-} // measurement
+static constexpr uint16_t BUFFER_SIZE = 60, NUM_PORTS = 8, VOLTAGE_PORT = 7;
 
 static void Set74HC4051_code(uint8_t c) {
   digitalWrite(D0, c & 1);
   digitalWrite(D1, (c >> 1) & 1);
   digitalWrite(D2, (c >> 2) & 1);
-  Serial.print(F("Port = "));
-  Serial.println(int(c));
+  // Serial.print(F("Port = "));
+  // Serial.println(int(c));
+  delayMicroseconds(10);
 } // Set74HC4051_code
+
+static float ProdIntegral[NUM_PORTS], VoltIntegral[NUM_PORTS],
+    CrntIntegral[NUM_PORTS];
+static uint16_t SampleCount;
+
+static void sample_all_ports() {
+  // each time we go over all the ports and add product to ProdIntergral
+  for (uint8_t PortI = 0; PortI < NUM_PORTS; PortI++) {
+    Set74HC4051_code(PortI);
+    float Current = analogRead(ANALOG_PIN);
+    Set74HC4051_code(VOLTAGE_PORT);
+    float Voltage = analogRead(ANALOG_PIN);
+    ProdIntegral[PortI] += Voltage * Current;
+    VoltIntegral[PortI] += Voltage;
+    CrntIntegral[PortI] += Current;
+    yield();
+  }
+  SampleCount++;
+} // sample_all_ports
 
 void setup() {
   initHardware();
@@ -96,14 +111,14 @@ void setup() {
   pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT);
 
-  Set74HC4051_code(7); // just to test
-  pinMode(LED_PIN,OUTPUT);
-  digitalWrite(LED_PIN,0);
+Set74HC4051_code(VOLTAGE_PORT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, 0);
 } // setup
 
 static WiFiClient client;
 
-static void send_back(const String &message) {
+static void reply(const String &message) {
   String s(F("HTTP/1.1 200 OK\r\n"
              "Content-Type: text/html\r\n\r\n"
              "<!DOCTYPE HTML>\r\n<html>\r\n"));
@@ -113,21 +128,27 @@ static void send_back(const String &message) {
   delay(1);
   Serial.println("Client disconnected.");
   client.stop();
-} // send_back
+} // reply
 
 void loop() {
-  if (client) { // client is still waiting data
-    // let's see whether we have finished measurement
-    if (CurEntry == MeasBuffer + BUFFER_SIZE) {
-      String s;
-      for (uint16_t si = 0; si < BUFFER_SIZE; si++) {
-        s += String(MeasBuffer[si].Time) + ", " + String(MeasBuffer[si].ADC) +
-             "<br>";
+  static uint32_t SamplingEnd;
+
+  if (client) { // client is still waiting for data
+    // let's see whether we have finished sample_all_ports
+    if (millis() - SamplingEnd < (1UL << 31)) {
+      String s(String(SampleCount) +
+               "<br>----------------------------------<br>");
+      for (uint8_t PortI = 0; PortI < NUM_PORTS; PortI++) {
+        float Pwr = /*ProdIntegral[PortI]; */(ProdIntegral[PortI] -
+                     CrntIntegral[PortI] * VoltIntegral[PortI] / SampleCount) /
+                    SampleCount;
+        s += String(Pwr) + "<br>";
+        ProdIntegral[PortI] = CrntIntegral[PortI] = VoltIntegral[PortI] = 0;
       }
-      CurEntry = MeasBuffer;
-      send_back(s);
+      SampleCount = 0;
+      reply(s);
     } else
-      measurement();
+      sample_all_ports();
   } else {
     // Check if a client has connected
     client = server.available();
@@ -138,18 +159,24 @@ void loop() {
       client.flush();
 
       // parse request
-      static int Port = -1;
+      int Port;
 
-      if ((Port = req.lastIndexOf("/port")) != -1) {
+      if (req.lastIndexOf("/read") != -1) {
+        // let's read port number
+        SamplingEnd = millis() + 1000;
+      } else if ((Port = req.lastIndexOf("/port")) != -1) {
         // let's read port number
         Port = (req.charAt(Port + strlen("/port/")) - '0');
         if (Port < 0 || Port > 7) {
-          send_back(String(F("Wrong port number! <br>")));
-        } else
+          reply(String(F("Wrong port number! <br>")));
+        } else {
           Set74HC4051_code(uint8_t(Port));
-      } else {
-        send_back(String(F("Invalid Request.<br> Try /port/<port>.")));
-      }
+          String s("Port  =");
+          s += String(Port) + ", " + String(analogRead(ANALOG_PIN));
+          reply(s);
+        }
+      } else
+        reply(String(F("Invalid Request.<br> Try /read.")));
     }
   }
 }
