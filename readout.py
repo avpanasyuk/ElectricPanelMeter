ffc502712e62e71898525d726b1cd0477f908dd6
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2018-02-04 17:24:54 -0500

    Works and records

--- readout.py
+++ readout.py
@@ -6,6 +6,20 @@ Created on Sat Feb  3 16:33:52 2018
 """
 
 import requests
-response = requests.get('http://192.168.2.169/read')
-response.text
+import time
 
+while True:
+  try:
+    response = requests.get('http://192.168.2.169/read')
+  except ConnectionError:
+    continue
+  t = response.text
+  open_ss = '<html>\r\n'
+  close_ss = '<br></html>'
+  t = t[t.find(open_ss) ++ len(open_ss):t.find(close_ss)]
+  t = t.replace('<br>',',')
+  with open('f:\\file.txt', 'a') as file:
+    file.write(str(time.time()) + ',' + t + '\n')
+  time.sleep(5)
+  
+  
