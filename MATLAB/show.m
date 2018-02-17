72f7d8a1f7d63d66393aba4e6fab1574102e5166
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2018-02-17 16:11:37 -0500

    Wrote show.m script

---
+++ MATLAB/show.m
@@ -0,0 +1,22 @@
+%% display results
+m = csvread('q:\PowerMon1.txt');
+serial_date = datenum(1970,1,1) + m(:,1)/60/60/24;
+
+% let's mark breaks > 10 min
+breaks = unique([1;find(AVP.diff(m(:,1)) > 600)+1;size(m,1)+1]);
+
+kWh = 0; Hrs = 0;
+for brI=1:numel(breaks)-1
+  brInds = [breaks(brI):breaks(brI+1)-1];
+  kWh = kWh + trapz(m(brInds,1),abs(m(brInds,2:end)))/60/60/1000;
+  Hrs = Hrs + (m(brInds(end),1) - m(brInds(1),1))/60/60;
+end
+
+price = kWh/2129*443/Hrs*30*24
+
+serial_date(breaks(2:end-1)) = NaN;
+
+plot(serial_date,abs(m(:,2:end)))
+datetick
+AVP.PLOT.legend(cellstr([num2str([1:12;price].')]))
+
